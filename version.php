24edd57623bc837920697cf4c866f7826656277f
Cabané <jorgecabane93@gmail.com>
2016-07-12 12:21:28 -0400

    version

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-    $plugin->version   = '2016071103';
+    $plugin->version   = '2016071104';
     $plugin->requires = 2010112400;
     $plugin->component = 'block_uai'; // To check on upgrade, that module sits in correct place
     
